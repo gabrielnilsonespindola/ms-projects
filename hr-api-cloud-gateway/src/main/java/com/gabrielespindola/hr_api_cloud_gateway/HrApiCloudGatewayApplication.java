4ca20db7338348acc71420f66551cd130105ce0d
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-20 16:58:47 -0300

    Project hr-api-gateway-cloud created

---
+++ hr-api-cloud-gateway/src/main/java/com/gabrielespindola/hr_api_cloud_gateway/HrApiCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielespindola.hr_api_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ hr-api-cloud-gateway/src/test/java/com/gabrielespindola/hr_api_cloud_gateway/HrApiCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielespindola.hr_api_cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
