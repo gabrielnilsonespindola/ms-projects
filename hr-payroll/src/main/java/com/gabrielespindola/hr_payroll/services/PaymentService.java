128ed83364eba58b85de07be81c59290cfd91fd6
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-19 23:28:07 -0300

    hr-payroll mock

---
+++ hr-payroll/src/main/java/com/gabrielespindola/hr_payroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.gabrielespindola.hr_payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.gabrielespindola.hr_payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
