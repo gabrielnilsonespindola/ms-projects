ac46ab979b646fb2a43a6bd70d8841fe932d549d
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-20 10:05:04 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/gabrielespindola/hr_payroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/gabrielespindola/hr_payroll/feignclients/WorkerFeignClient.java
@@ -11,7 +11,7 @@ import com.gabrielespindola.hr_payroll.entities.Worker;
 
 
 @Component
-@FeignClient(name = "hr-worker" , url = "localhost:8001" , path = "/workers")
+@FeignClient(name = "hr-worker" ,  path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "/{id}")
